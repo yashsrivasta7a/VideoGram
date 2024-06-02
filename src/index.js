54845e9a4e377bd58adb9c8f3517a72bb450edba
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2024-06-02 22:13:47 +0530

    databse connection error verification

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from "express";
+
+const app = express();
 
 dotenv.config ({
     path:"./env"
 })
 
-connectDB();
+connectDB().then(()=>{
+    app.on("error",(error)=>{
+        console.log(`error:${error}`);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000 , 
+        ()=>{
+            console.log(`server is runing at PORT : ${process.env.PORT}` );
+        })
+})
+.catch((error)=>{
+    console.log(`Connection failed with DataBase`,error);
+})
