a71930bc2d7911c2f71be3be2758cfe1932c6195
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2024-06-04 17:12:05 +0530

    multer middleware

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,3 +1,13 @@
 import multer from "multer";
 
-const upload = 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+
+      cb(null, file.originalname)
+    }
+  })
+  
+  export const upload = multer({ storage, })
