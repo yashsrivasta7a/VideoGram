9f7320a4139104ffa30ad8a7d91250b16f59ffb4
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2024-07-06 23:11:08 +0530

    subcription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // jo subscribe krra hai
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // to whom subscribing
+      ref: "User",
+    },
+  },
+  {
+     timestamps: true 
+}
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
